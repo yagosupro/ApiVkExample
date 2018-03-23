12ec890ce1283d6158ff245253e033ab0213e677
yagosupro <yagosupro@gmail.com>
2018-03-23 11:47:04 +0300

    test
    test

--- app/src/main/java/com/example/bob_book/apivkexample/MainActivity.java
+++ app/src/main/java/com/example/bob_book/apivkexample/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+//import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,6 +24,12 @@ import com.example.bob_book.apivkexample.Model.EventBusModel.EventMessage;
 import com.example.bob_book.apivkexample.Model.Response;
 import com.example.bob_book.apivkexample.Realm.ItemRealm;
 import com.google.gson.Gson;
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
@@ -172,6 +179,26 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         return super.onOptionsItemSelected(item);
     }
 
+//    public void draweCustom(Toolbar toolbar){
+//        new Drawer()
+//            .withActivity(this)
+//            .withToolbar(toolbar)
+//            .withActionBarDrawerToggle(true)
+//            .withHeader(R.layout.drawer_header)
+//            .addDrawerItems(
+//                    new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home).withBadge("99").withIdentifier(1),
+//                    new PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
+//                    new PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye).withBadge("6").withIdentifier(2),
+//                    new SectionDrawerItem().withName(R.string.drawer_item_settings),
+//                    new SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_cog),
+//                    new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_question).setEnabled(false),
+//                    new DividerDrawerItem(),
+//                    new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_github).withBadge("12+").withIdentifier(1)
+//            )
+//            .build();
+//
+//    }
+
     ////////////////////////////////////////////////////////////////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -182,6 +209,12 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         rv = (RecyclerView) findViewById(R.id.rv);
         btnSave = (Button) findViewById(R.id.save);
         btnLoad = (Button) findViewById(R.id.load);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        draweCustom(toolbar);
+
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
